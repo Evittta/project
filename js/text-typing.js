88ef00a333968cc2a5aba57db2d2711fa988ded9
evita kusinya <vita.kusinya@gmail.com>
2017-11-13 12:55:08 +0000

    style

---
+++ js/text-typing.js
@@ -0,0 +1,12 @@
+function writeText(id, text, speed){
+	let el = document.getElementById(id), txt = text.join("").split("");
+    let interval = setInterval(function() {
+    	if(!txt[0]) {
+    		return clearInterval(interval);
+    	};
+		el.innerHTML += txt.shift();
+    }, speed != undefined ? speed : 150);
+    return false;
+};
+    
+writeText("text-type", ["Evita Kusinya"]);
